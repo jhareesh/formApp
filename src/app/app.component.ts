0bc691789fbb31b252a82d265cb0f70d4aa7bcbe
FullStackDeveloper-j <128317139+FullStackDeveloper-j@users.noreply.github.com>
2023-05-02 03:38:45 +0530

    done displaying in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formApp';
+
+
+  //defing an variables to assign to ngmodel 
+  name1:any
+  phone1:any
+  email1:any
+  //definig an function to give in (ngSubmit)="fun(userform.value)" for the form
+  fun(data:any)
+  {
+    console.log(data)
+    console.log(this.name1)
+    console.log(this.phone1)
+    console.log(this.email1)
+    //displays the results in the console
+  }
 }
